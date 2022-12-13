39047929e0491d0ecc52539ed93f04557fe7ab08
diasvixub <113109541+diasvixub@users.noreply.github.com>
2022-12-14 00:23:57 +0400

    Add files via upload

--- setup_custom.py
+++ setup_custom.py
@@ -4,46 +4,13 @@ from subprocess import run, DEVNULL
 import sys
 from progress.bar import IncrementalBar
 
-# Переменные
 path = 'C:\\INST'
 p_name = '                                   WIN7-SCRIPT'
 hr = '═══════════════════════════════════════════════════════════════════════════════'
-updates_list = ['Updates\\windowsupdateagent-7.6-x64.exe', 'Updates\\Windows6.1-KB3102810-x64.msu',
-                'Updates\\Windows6.1-kb2533552-x64.msu', 'Updates\\Windows6.1-kb2545698-x64.msu',
-                'Updates\\Windows6.1-kb2547666-x64.msu', 'Updates\\Windows6.1-kb2574819-v2-x64.msu',
-                'Updates\\Windows6.1-kb2603229-x64.msu', 'Updates\\Windows6.1-kb2667402-v2-x64.msu',
-                'Updates\\Windows6.1-kb2670838-x64.msu', 'Updates\\Windows6.1-kb2676562-x64.msu',
-                'Updates\\Windows6.1-kb2685811-x64.msu', 'Updates\\Windows6.1-kb2685813-x64.msu',
-                'Updates\\Windows6.1-kb2698365-x64.msu', 'Updates\\Windows6.1-kb2706045-x64.msu',
-                'Updates\\Windows6.1-kb2729094-v2-x64.msu', 'Updates\\Windows6.1-kb2732059-v5-x64.msu',
-                'Updates\\Windows6.1-kb2750841-x64.msu', 'Updates\\Windows6.1-kb2761217-x64.msu',
-                'Updates\\Windows6.1-kb2773072-x64.msu', 'Updates\\Windows6.1-kb2813347-x64.msu',
-                'Updates\\Windows6.1-kb2830477-x64.msu', 'Updates\\Windows6.1-kb2834140-v2-x64.msu',
-                'Updates\\Windows6.1-kb2862330-v2-x64.msu', 'Updates\\Windows6.1-kb2894844-x64.msu',
-                'Updates\\Windows6.1-kb2900986-x64.msu', 'Updates\\Windows6.1-kb2919469-x64.msu',
-                'Updates\\Windows6.1-kb2952664-v25-x64.msu', 'Updates\\Windows6.1-kb2970228-x64.msu',
-                'Updates\\Windows6.1-kb2984972-x64.msu', 'Updates\\Windows6.1-kb3004375-v3-x64.msu',
-                'Updates\\Windows6.1-kb3006137-x64.msu', 'Updates\\Windows6.1-kb3020369-x64.msu',
-                'Updates\\Windows6.1-kb3021917-x64.msu', 'Updates\\Windows6.1-kb3046269-x64.msu',
-                'Updates\\Windows6.1-kb3059317-x64.msu', 'Updates\\Windows6.1-kb3068708-x64.msu',
-                'Updates\\Windows6.1-kb3080149-x64.msu', 'Updates\\Windows6.1-kb3102429-v2-x64.msu',
-                'Updates\\Windows6.1-kb3118401-x64.msu', 'Updates\\Windows6.1-kb3123479-x64.msu',
-                'Updates\\Windows6.1-kb3125574-v4-x64.msu', 'Updates\\Windows6.1-kb3138612-x64.msu',
-                'Updates\\Windows6.1-kb3140245-x64.msu', 'Updates\\Windows6.1-kb3150513-x64.msu',
-                'Updates\\Windows6.1-kb3156016-x64.msu', 'Updates\\Windows6.1-kb3159398-x64.msu',
-                'Updates\\Windows6.1-kb3161102-x64.msu', 'Updates\\Windows6.1-kb3161949-x64.msu',
-                'Updates\\Windows6.1-kb3172605-x64.msu', 'Updates\\Windows6.1-kb3179573-x64.msu',
-                'Updates\\Windows6.1-kb3184143-x64.msu', 'Updates\\Windows6.1-KB4019990-x64.msu',
-                'Updates\\Windows6.1-kb4040980-x64.msu', 'Updates\\Windows6.1-kb4088878-x64.msu',
-                'Updates\\Windows6.1-kb4099950-x64.msu', 'Updates\\Windows6.1-kb4474419-v3-x64.msu',
-                'Updates\\Windows6.1-kb4490628-x64.msu', 'Updates\\Windows6.1-kb4524752-x64.msu',
-                'Updates\\Windows6.1-kb5010798-x64.msu', 'Updates\\Windows6.1-kb5013637-x64.msu',
-                'Updates\\Windows6.1-kb5017397-x64.msu', 'Updates\\Windows6.1-kb5018454-x64.msu',
-                'Updates\\Windows6.1-kb5020000-x64.msu']
-certs_list = ['Certs\\rootsupd.exe', 'Certs\\updroots.exe', 'Certs\\roots.sst', 'Certs\\']
-libs_list = ['Libs\\dxwebsetup.exe', 'Libs\\ndp48-x86-x64-allos-enu.exe',
-             'Libs\\VisualCppRedist_AIO_x86_x64.exe']
+updates_list = listdir('Updates')
+libs_list = listdir('Libs')
 programs_list = listdir('Programs')
+certs_list = ['Certs\\rootsupd.exe', 'Certs\\updroots.exe', 'Certs\\roots.sst', 'Certs\\']
 dism_list = ['/FeatureName:WindowsGadgetPlatform ', '/FeatureName:InboxGames', '/FeatureName:More Games',
              '/FeatureName:Solitaire', '/FeatureName:SpiderSolitaire', '/FeatureName:Hearts', '/FeatureName:FreeCell',
              '/FeatureName:Minesweeper', '/FeatureName:PurplePlace']
@@ -51,6 +18,7 @@ sc_list = ['wuauserv', 'WSearch', 'SDRSVC', 'TabletInputService', 'bthserv', 'BD
 
 
 def main():
+    system('title MENU')
     try:
         chdir(path)
     except FileNotFoundError:
@@ -107,55 +75,71 @@ def product():
 
 
 def copy():
+    system('title COPYING FILES...')
     print('Copying files...')
-    run(['xcopy', '*', path, '/Y', '/S'], shell=False, stdout=DEVNULL)
+    run(['xcopy', '*', path, '/E', '/Y'], shell=False, stdout=DEVNULL)
 
 
 def updates(a):
-    product()
-    yesno = input(f'{len(a)} updates will be installed. Continue? (y/n) ')
-    if yesno == 'y':
+    system('title UPDATES')
+    if len(a) > 0:  # If there are files in the directory
         product()
-        print('Enabling Windows Update...')
-        sleep(1)
-        print(hr)
-        run(['sc', 'config', sc_list[0], 'start=', 'demand'], shell=False, stdout=DEVNULL)
-        run(['net', 'start', sc_list[0]], shell=False, stdout=DEVNULL)
-        product()
-        bar = IncrementalBar('Installing updates', max=len(updates_list))
-        for i in range(0, len(a)):
-            if i == 0:
-                run([updates_list[i]], shell=False, stdout=DEVNULL)
+        yesno = input(f'{len(a)} updates will be installed. Continue? (y/n) ')
+        if yesno == 'y':
+            system('title ENABLING WINDOWS UPDATE')
+            product()
+            print('Enabling Windows Update...')
+            sleep(1)
+            print(hr)
+            run(['sc', 'config', sc_list[0], 'start=', 'demand'], shell=False, stdout=DEVNULL)
+            run(['net', 'start', sc_list[0]], shell=False, stdout=DEVNULL)
+            product()
+            bar = IncrementalBar('Installing updates', max=len(a))
+            for i in range(0, len(a)):
                 bar.next()
-            else:
-                run(['wusa.exe', updates_list[i], '/quiet', '/norestart'], shell=False, stdout=DEVNULL)
-                bar.next()
-        bar.finish()
-        sleep(1)
+                try:
+                    run(f'wusa.exe Updates\\{a[i]} /quiet /norestart', shell=True, stdout=DEVNULL)
+                except FileNotFoundError:
+                    pass
+            bar.finish()
+            sleep(1)
+            product()
+            print('Installation was successful')
+            sleep(1)
+            main()
+        elif yesno == 'n':
+            main()
+        else:
+            product()
+            print('Confirm (y) or reject (n) the installation')
+            sleep(2)
+            updates(updates_list)
+    else:  # If there are no files in the directory
         product()
-        print('Installation was successful')
+        print('Installers not found')
         sleep(1)
         main()
-    elif yesno == 'n':
-        main()
-    else:
-        product()
-        print('Confirm (y) or reject (n) the installation')
-        sleep(2)
-        updates(updates_list)
 
 
 def certs(a):
+    system('title CERTS')
     product()
     yesno = input('Root certificates will be updated. Continue? (y/n) ')
     if yesno == 'y':
+        system('title UPDATING ROOT CERTIFICATES')
         product()
         print('Updating root certificates...')
         print(hr)
         print('Click "No" in the windows that appear on the screen')
-        run(f'{a[0]} /c /t:C:\\INST\\{a[3]}', shell=True, stdout=DEVNULL)
+        try:
+            run(f'{a[0]} /c /t:C:\\INST\\{a[3]}', shell=True, stdout=DEVNULL)
+        except FileNotFoundError:
+            pass
         sleep(1)
-        run([a[1], a[2]], shell=False, stdout=DEVNULL)
+        try:
+            run([a[1], a[2]], shell=False, stdout=DEVNULL)
+        except FileNotFoundError:
+            pass
         product()
         print('Certificate update completed successfully')
         sleep(1)
@@ -170,39 +154,55 @@ def certs(a):
 
 
 def libs(a):
-    product()
-    yesno = input('Libraries will be installed. Continue? (y/n) ')
-    if yesno == 'y':
+    system('title LIBS')
+    if len(a) > 0:
         product()
-        bar = IncrementalBar('Installing libraries', max=len(a))
-        for i in range(0, len(a)):
-            run([a[i]], shell=False, stdout=DEVNULL)
-            bar.next()
-        bar.finish()
-        sleep(1)
-        product()
-        print('Installation was successful')
-        sleep(1)
-        main()
-    elif yesno == 'n':
-        main()
+        yesno = input('Libraries will be installed. Continue? (y/n) ')
+        if yesno == 'y':
+            system('title INSTALLING LIBRARIES...')
+            product()
+            bar = IncrementalBar('Installing libraries', max=len(a))
+            for i in range(0, len(a)):
+                bar.next()
+                try:
+                    run(f'Libs\\{a[i]}', shell=True, stdout=DEVNULL)
+                except FileNotFoundError:
+                    pass
+            bar.finish()
+            sleep(1)
+            product()
+            print('Installation was successful')
+            sleep(1)
+            main()
+        elif yesno == 'n':
+            main()
+        else:
+            product()
+            print('Confirm (y) or reject (n) the installation')
+            sleep(2)
+            libs(libs_list)
     else:
         product()
-        print('Confirm (y) or reject (n) the installation')
-        sleep(2)
-        libs(libs_list)
+        print('Installers not found')
+        sleep(1)
+        main()
 
 
 def programs(a):
+    system(f'title PROGRAMS')
     if len(a) > 0:
         product()
         yesno = input('Programs will be installed. Continue? (y/n) ')
         if yesno == 'y':
+            system('title INSTALLING PROGRAMS...')
             product()
             bar = IncrementalBar('Installing programs', max=len(a))
             for i in range(0, len(a)):
-                run(f'Programs\\{a[i]}', shell=True, stdout=DEVNULL)
                 bar.next()
+                try:
+                    run(f'Programs\\{a[i]}', shell=True, stdout=DEVNULL)
+                except FileNotFoundError:
+                    pass
             bar.finish()
             sleep(1)
             product()
@@ -224,9 +224,11 @@ def programs(a):
 
 
 def boost(a, b):
+    system('title OPTIMIZING')
     product()
     yesno = input('Windows will be optimized. Continue? (y/n) ')
     if yesno == 'y':
+        system('title OPTIMIZATION...')
         product()
         bar = IncrementalBar('Optimization', max=len(a)+len(b)+3)
         for i in range(0, len(a)):
@@ -260,9 +262,11 @@ def boost(a, b):
 
 
 def clean():
+    system('title CLEANING TEMPORARY FILES')
     product()
     yesno = input('Temporary files will be deleted. Continue? (y/n) ')
     if yesno == 'y':
+        system('title CLEANING...')
         product()
         bar = IncrementalBar('Cleaning', max=6)
         run('del C:\\Windows\\SoftwareDistribution\\Download\\*.* /s /q >nul 2>&1', shell=True, stdout=DEVNULL)
--- setup_final.py
+++ setup_final.py
@@ -4,71 +4,101 @@ from subprocess import run, DEVNULL
 import sys
 from progress.bar import IncrementalBar
 
+# Переменные
 path = 'C:\\INST'
 p_name = '                                   WIN7-SCRIPT'
 hr = '═══════════════════════════════════════════════════════════════════════════════'
-updates_list = listdir('Updates')
-libs_list = listdir('Libs')
-programs_list = listdir('Programs')
+updates_list = ['Updates\\windowsupdateagent-7.6-x64.exe', 'Updates\\Windows6.1-KB3102810-x64.msu',
+                'Updates\\Windows6.1-kb2533552-x64.msu', 'Updates\\Windows6.1-kb2545698-x64.msu',
+                'Updates\\Windows6.1-kb2547666-x64.msu', 'Updates\\Windows6.1-kb2574819-v2-x64.msu',
+                'Updates\\Windows6.1-kb2603229-x64.msu', 'Updates\\Windows6.1-kb2667402-v2-x64.msu',
+                'Updates\\Windows6.1-kb2670838-x64.msu', 'Updates\\Windows6.1-kb2676562-x64.msu',
+                'Updates\\Windows6.1-kb2685811-x64.msu', 'Updates\\Windows6.1-kb2685813-x64.msu',
+                'Updates\\Windows6.1-kb2698365-x64.msu', 'Updates\\Windows6.1-kb2706045-x64.msu',
+                'Updates\\Windows6.1-kb2729094-v2-x64.msu', 'Updates\\Windows6.1-kb2732059-v5-x64.msu',
+                'Updates\\Windows6.1-kb2750841-x64.msu', 'Updates\\Windows6.1-kb2761217-x64.msu',
+                'Updates\\Windows6.1-kb2773072-x64.msu', 'Updates\\Windows6.1-kb2813347-x64.msu',
+                'Updates\\Windows6.1-kb2830477-x64.msu', 'Updates\\Windows6.1-kb2834140-v2-x64.msu',
+                'Updates\\Windows6.1-kb2862330-v2-x64.msu', 'Updates\\Windows6.1-kb2894844-x64.msu',
+                'Updates\\Windows6.1-kb2900986-x64.msu', 'Updates\\Windows6.1-kb2919469-x64.msu',
+                'Updates\\Windows6.1-kb2952664-v25-x64.msu', 'Updates\\Windows6.1-kb2970228-x64.msu',
+                'Updates\\Windows6.1-kb2984972-x64.msu', 'Updates\\Windows6.1-kb3004375-v3-x64.msu',
+                'Updates\\Windows6.1-kb3006137-x64.msu', 'Updates\\Windows6.1-kb3020369-x64.msu',
+                'Updates\\Windows6.1-kb3021917-x64.msu', 'Updates\\Windows6.1-kb3046269-x64.msu',
+                'Updates\\Windows6.1-kb3059317-x64.msu', 'Updates\\Windows6.1-kb3068708-x64.msu',
+                'Updates\\Windows6.1-kb3080149-x64.msu', 'Updates\\Windows6.1-kb3102429-v2-x64.msu',
+                'Updates\\Windows6.1-kb3118401-x64.msu', 'Updates\\Windows6.1-kb3123479-x64.msu',
+                'Updates\\Windows6.1-kb3125574-v4-x64.msu', 'Updates\\Windows6.1-kb3138612-x64.msu',
+                'Updates\\Windows6.1-kb3140245-x64.msu', 'Updates\\Windows6.1-kb3150513-x64.msu',
+                'Updates\\Windows6.1-kb3156016-x64.msu', 'Updates\\Windows6.1-kb3159398-x64.msu',
+                'Updates\\Windows6.1-kb3161102-x64.msu', 'Updates\\Windows6.1-kb3161949-x64.msu',
+                'Updates\\Windows6.1-kb3172605-x64.msu', 'Updates\\Windows6.1-kb3179573-x64.msu',
+                'Updates\\Windows6.1-kb3184143-x64.msu', 'Updates\\Windows6.1-KB4019990-x64.msu',
+                'Updates\\Windows6.1-kb4040980-x64.msu', 'Updates\\Windows6.1-kb4088878-x64.msu',
+                'Updates\\Windows6.1-kb4099950-x64.msu', 'Updates\\Windows6.1-kb4474419-v3-x64.msu',
+                'Updates\\Windows6.1-kb4490628-x64.msu', 'Updates\\Windows6.1-kb4524752-x64.msu',
+                'Updates\\Windows6.1-kb5010798-x64.msu', 'Updates\\Windows6.1-kb5013637-x64.msu',
+                'Updates\\Windows6.1-kb5017397-x64.msu', 'Updates\\Windows6.1-kb5018454-x64.msu',
+                'Updates\\Windows6.1-kb5020000-x64.msu']
 certs_list = ['Certs\\rootsupd.exe', 'Certs\\updroots.exe', 'Certs\\roots.sst', 'Certs\\']
+libs_list = ['Libs\\dxwebsetup.exe', 'Libs\\ndp48-x86-x64-allos-enu.exe',
+             'Libs\\VisualCppRedist_AIO_x86_x64.exe']
+programs_list = listdir('Programs')
 dism_list = ['/FeatureName:WindowsGadgetPlatform ', '/FeatureName:InboxGames', '/FeatureName:More Games',
              '/FeatureName:Solitaire', '/FeatureName:SpiderSolitaire', '/FeatureName:Hearts', '/FeatureName:FreeCell',
              '/FeatureName:Minesweeper', '/FeatureName:PurplePlace']
 sc_list = ['wuauserv', 'WSearch', 'SDRSVC', 'TabletInputService', 'bthserv', 'BDESVC', 'VSS', 'SCardSvr', 'WinDefend']
 
 
-def start():
-    try:  # Attempt to go to the directory
-        chdir(path)
-    except FileNotFoundError:  # In the absence of a directory
-        mkdir(path)  # Create a directory
-        product()
-        copy()  # Copy files from the current directory
-        system(f'start {path}\\setup')  # Run the program from the created directory
-        sys.exit()
-    else:
-        main()
-
-
 def main():
-    system('chcp 866')
-    product()
-    print('1 Installing Windows Updates')
-    print('2 Updating Root Certificates')
-    print('3 Installing Libraries')
-    print('4 Installing Programs')
-    print('5 Optimizing Windows')
-    print('6 Cleaning Temporary Files')
-    print('7 Get Help')
-    print('8 Restart Windows')
-    print('0 Exit the program')
-    print(hr)
-    operation = input('Operation number: ')
-    system('cls')
-    if operation == '1':
-        updates(updates_list)
-    elif operation == '2':
-        certs(certs_list)
-    elif operation == '3':
-        libs(libs_list)
-    elif operation == '4':
-        programs(programs_list)
-    elif operation == '5':
-        boost(dism_list, sc_list)
-    elif operation == '6':
-        clean()
-    elif operation == '7':
-        system('notepad readme.txt')
-        main()
-    elif operation == '8':
-        run(['shutdown', '/r', '/t', '5'], shell=False, stdout=DEVNULL)
-    elif operation == '0':
+    system('title MENU')
+    try:
+        chdir(path)
+    except FileNotFoundError:
+        mkdir(path)
+        product()
+        copy()
+        system(f'start {path}\\setup')
         sys.exit()
     else:
+        system('chcp 866')
         product()
-        print('Invalid operation number entered')
-        sleep(1)
-        main()
+        print('1 Installing Windows Updates')
+        print('2 Updating Root Certificates')
+        print('3 Installing Libraries')
+        print('4 Installing Programs')
+        print('5 Optimizing Windows')
+        print('6 Cleaning Temporary Files')
+        print('7 Get Help')
+        print('8 Restart Windows')
+        print('0 Exit the program')
+        print(hr)
+        operation = input('Operation number: ')
+        system('cls')
+        if operation == '1':
+            updates(updates_list)
+        elif operation == '2':
+            certs(certs_list)
+        elif operation == '3':
+            libs(libs_list)
+        elif operation == '4':
+            programs(programs_list)
+        elif operation == '5':
+            boost(dism_list, sc_list)
+        elif operation == '6':
+            clean()
+        elif operation == '7':
+            system('notepad readme.txt')
+            main()
+        elif operation == '8':
+            run(['shutdown', '/r', '/t', '5'], shell=False, stdout=DEVNULL)
+        elif operation == '0':
+            sys.exit()
+        else:
+            product()
+            print('Invalid operation number entered')
+            sleep(1)
+            main()
 
 
 def product():
@@ -78,57 +108,73 @@ def product():
 
 
 def copy():
+    system('title COPYING FILES...')
     print('Copying files...')
     run(['xcopy', '*', path, '/E', '/Y'], shell=False, stdout=DEVNULL)
 
 
 def updates(a):
-    if len(a) > 0:  # If there are files in the directory
+    system('title UPDATES')
+    product()
+    yesno = input(f'{len(a)} updates will be installed. Continue? (y/n) ')
+    if yesno == 'y':
+        system('title ENABLING WINDOWS UPDATE')
         product()
-        yesno = input(f'{len(a)} updates will be installed. Continue? (y/n) ')
-        if yesno == 'y':
-            product()
-            print('Enabling Windows Update...')
-            sleep(1)
-            print(hr)
-            run(['sc', 'config', sc_list[0], 'start=', 'demand'], shell=False, stdout=DEVNULL)
-            run(['net', 'start', sc_list[0]], shell=False, stdout=DEVNULL)
-            product()
-            bar = IncrementalBar('Installing updates', max=len(updates_list))
-            for i in range(0, len(a)):
-                run(f'wusa.exe Updates\\{updates_list[i]} /quiet /norestart', shell=True, stdout=DEVNULL)
-                bar.next()
-            bar.finish()
-            sleep(1)
-            product()
-            print('Installation was successful')
-            sleep(1)
-            main()
-        elif yesno == 'n':
-            main()
-        else:
-            product()
-            print('Confirm (y) or reject (n) the installation')
-            sleep(2)
-            updates(updates_list)
-    else:  # If there are no files in the directory
+        print('Enabling Windows Update...')
+        sleep(1)
+        print(hr)
+        run(['sc', 'config', sc_list[0], 'start=', 'demand'], shell=False, stdout=DEVNULL)
+        run(['net', 'start', sc_list[0]], shell=False, stdout=DEVNULL)
         product()
-        print('Installers not found')
+        bar = IncrementalBar('Installing updates', max=len(a))
+        for i in range(0, len(a)):
+            bar.next()
+            if i == 0:
+                system('title INSTALLING WINDOWS AGENT...')
+                try:
+                    run([a[i]], shell=False, stdout=DEVNULL)
+                except FileNotFoundError:
+                    pass
+            else:
+                system(f'title INSTALLING {a[i][19:28]}...')
+                try:
+                    run(['wusa.exe', a[i], '/quiet', '/norestart'], shell=False, stdout=DEVNULL)
+                except FileNotFoundError:
+                    pass
+        bar.finish()
+        sleep(1)
+        product()
+        print('Installation was successful')
         sleep(1)
         main()
+    elif yesno == 'n':
+        main()
+    else:
+        product()
+        print('Confirm (y) or reject (n) the installation')
+        sleep(2)
+        updates(updates_list)
 
 
 def certs(a):
+    system('title CERTS')
     product()
     yesno = input('Root certificates will be updated. Continue? (y/n) ')
     if yesno == 'y':
+        system('title UPDATING ROOT CERTIFICATES')
         product()
         print('Updating root certificates...')
         print(hr)
         print('Click "No" in the windows that appear on the screen')
-        run(f'{a[0]} /c /t:C:\\INST\\{a[3]}', shell=True, stdout=DEVNULL)
+        try:
+            run(f'{a[0]} /c /t:C:\\INST\\{a[3]}', shell=True, stdout=DEVNULL)
+        except FileNotFoundError:
+            pass
         sleep(1)
-        run([a[1], a[2]], shell=False, stdout=DEVNULL)
+        try:
+            run([a[1], a[2]], shell=False, stdout=DEVNULL)
+        except FileNotFoundError:
+            pass
         product()
         print('Certificate update completed successfully')
         sleep(1)
@@ -143,45 +189,49 @@ def certs(a):
 
 
 def libs(a):
-    if len(a) > 0:
+    system('title LIBS')
+    product()
+    yesno = input('Libraries will be installed. Continue? (y/n) ')
+    if yesno == 'y':
+        system('title INSTALLING LIBRARIES...')
         product()
-        yesno = input('Libraries will be installed. Continue? (y/n) ')
-        if yesno == 'y':
-            product()
-            bar = IncrementalBar('Installing libraries', max=len(a))
-            for i in range(0, len(a)):
-                run(f'Libs\\{a[i]}', shell=True, stdout=DEVNULL)
-                bar.next()
-            bar.finish()
-            sleep(1)
-            product()
-            print('Installation was successful')
-            sleep(1)
-            main()
-        elif yesno == 'n':
-            main()
-        else:
-            product()
-            print('Confirm (y) or reject (n) the installation')
-            sleep(2)
-            libs(libs_list)
-    else:
+        bar = IncrementalBar('Installing libraries', max=len(a))
+        for i in range(0, len(a)):
+            bar.next()
+            try:
+                run([a[i]], shell=False, stdout=DEVNULL)
+            except FileNotFoundError:
+                pass
+        bar.finish()
+        sleep(1)
         product()
-        print('Installers not found')
+        print('Installation was successful')
         sleep(1)
         main()
+    elif yesno == 'n':
+        main()
+    else:
+        product()
+        print('Confirm (y) or reject (n) the installation')
+        sleep(2)
+        libs(libs_list)
 
 
 def programs(a):
+    system(f'title PROGRAMS')
     if len(a) > 0:
         product()
         yesno = input('Programs will be installed. Continue? (y/n) ')
         if yesno == 'y':
+            system('title INSTALLING PROGRAMS...')
             product()
             bar = IncrementalBar('Installing programs', max=len(a))
             for i in range(0, len(a)):
-                run(f'Programs\\{a[i]}', shell=True, stdout=DEVNULL)
                 bar.next()
+                try:
+                    run(f'Programs\\{a[i]}', shell=True, stdout=DEVNULL)
+                except FileNotFoundError:
+                    pass
             bar.finish()
             sleep(1)
             product()
@@ -203,9 +253,11 @@ def programs(a):
 
 
 def boost(a, b):
+    system('title OPTIMIZING')
     product()
     yesno = input('Windows will be optimized. Continue? (y/n) ')
     if yesno == 'y':
+        system('title OPTIMIZATION...')
         product()
         bar = IncrementalBar('Optimization', max=len(a)+len(b)+3)
         for i in range(0, len(a)):
@@ -239,30 +291,26 @@ def boost(a, b):
 
 
 def clean():
+    system('title CLEANING TEMPORARY FILES')
     product()
     yesno = input('Temporary files will be deleted. Continue? (y/n) ')
     if yesno == 'y':
+        system('title CLEANING...')
         product()
         bar = IncrementalBar('Cleaning', max=6)
+        bar.next()
         run('del C:\\Windows\\SoftwareDistribution\\Download\\*.* /s /q >nul 2>&1', shell=True, stdout=DEVNULL)
         bar.next()
-        sleep(1)
         run('for /d %i in (C:\\Windows\\SoftwareDistribution\\Download\\*.*) do @rmdir /s /q "%i" >nul 2>&1',
             shell=True, stdout=DEVNULL)
         bar.next()
-        sleep(1)
         run('del C:\\Windows\\Prefetch\\*.* /s /q >nul 2>&1', shell=True, stdout=DEVNULL)
         bar.next()
-        sleep(1)
         run('for /d %i in (C:\\Windows\\Prefetch\\*.*) do @rmdir /s /q "%i" >nul 2>&1', shell=True, stdout=DEVNULL)
         bar.next()
-        sleep(1)
         run('del %temp%\\*.* /s /q >nul 2>&1', shell=True, stdout=DEVNULL)
         bar.next()
-        sleep(1)
         run('for /d %i in (%temp%\\*.*) do @rmdir /s /q "%i" >nul 2>&1', shell=True, stdout=DEVNULL)
-        bar.next()
-        sleep(1)
         bar.finish()
         sleep(1)
         product()
@@ -278,4 +326,4 @@ def clean():
         clean()
 
 
-start()
+main()
